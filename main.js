59c47bec53bf9c693515713a5f756702b641b9c2
Langel <knoxburry@gmail.com>
2025-01-07 21:52:56 -0500

    cleaned up console loggings

--- main.js
+++ main.js
@@ -8,8 +8,11 @@ console.log(tohex(69));
 
 fs.readdirSync('./nsf').forEach(file => {
 	if (path.extname(file) == '.nsf') {
- 		console.log(file);
+ 		console.log('\n=== ' + file);
 		let data = fs.readFileSync('./nsf/' + file);
-		nsf.process(data);
+		nsf.file_process(data);
 	}
 });
+
+console.log('\nEMPTY PAGES inside all processed NSFs:');
+console.log(nsf.empty_pages());
