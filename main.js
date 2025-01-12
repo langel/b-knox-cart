428655429ec71735621ac2bb312f21e09d5db6b2
Langel <knoxburry@gmail.com>
2025-01-11 20:53:59 -0500

    display free rom space and cli color control

--- main.js
+++ main.js
@@ -2,13 +2,13 @@ let fs = require('fs');
 let path = require('path');
 let nsf = require('./lib/nsf.js');
 
-let {tohex} = require('./lib/util.js');
+let {tohex, cliclr} = require('./lib/util.js');
 
 console.log(tohex(69));
 
 fs.readdirSync('./nsf').forEach(file => {
 	if (path.extname(file) == '.nsf') {
- 		console.log('\n=== ' + file);
+ 		console.log(cliclr('cyan', '\n=== ' + file));
 		let data = fs.readFileSync('./nsf/' + file);
 		nsf.file_process(data);
 	}
