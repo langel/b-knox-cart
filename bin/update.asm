87298333f7f20b9bb2433016f721227428f0024f
langel <knoxburry@gmail.com>
2025-01-14 14:48:51 -0500

    playback cycle length visualizer

--- bin/update.asm
+++ bin/update.asm
@@ -1,6 +1,21 @@
 
 update: subroutine
 
+	ldx #$00
+.wait_loop
+	nop
+	nop
+	nop
+	nop
+	dex
+	bne .wait_loop
+
+	lda #%00011001
+	sta ppu_mask
+
 	jsr nsf_play 
+
+	lda #%00011000
+	sta ppu_mask
 
 	rti
