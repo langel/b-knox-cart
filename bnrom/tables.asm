1968240a50ae75872367e5f1398f9b0f44337d2f
langel <knoxburry@gmail.com>
2025-03-05 20:29:28 -0500

    title screen work and better track lengths

--- bnrom/tables.asm
+++ bnrom/tables.asm
@@ -7,6 +7,6 @@ track_updates_lo:
 track_updates_hi:
 	hex b0 af ac b2 80 80 b4 80 b4 80 80 b0 80 b2 80 ac 
 track_lengths_lo:
-	hex 00 f0 60 10 30 2a a0 a0 90 a8 f0 30 20 18 98 3a 
+	hex 00 f8 60 10 30 32 a0 a0 90 b0 f0 50 20 30 98 3a 
 track_lengths_hi:
 	hex 17 23 30 0e 11 0f 1a 0b 0c 0a 0b 21 2a 10 11 31 
